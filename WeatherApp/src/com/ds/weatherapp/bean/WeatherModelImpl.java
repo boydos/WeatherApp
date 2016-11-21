efb86e181038927ce790184f1c810fbb243488f7
byds <tongheizi@163.com>
2016-11-22 00:55:20 +0800

    add weather api
    version 1.2

--- WeatherApp/src/com/ds/weatherapp/bean/WeatherModelImpl.java
+++ WeatherApp/src/com/ds/weatherapp/bean/WeatherModelImpl.java
@@ -1,16 +1,26 @@
 package com.ds.weatherapp.bean;
 
+import java.net.URLEncoder;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.http.util.EncodingUtils;
+
+import com.ds.utils.EncodeUtils;
 import com.ds.utils.NetWorkUtils;
 import com.ds.utils.StringUtils;
 import com.ds.weatherapp.control.OnWeatherListener;
 import com.ds.weatherapp.utils.IContants;
 
-public class WeatherModelImpl implements IWeatherModel {
+public class WeatherModelImpl implements IWeatherModel,IContants {
 
 	@Override
 	public void loadWeather(String cityNo, OnWeatherListener listener) {
 		// TODO Auto-generated method stub
-		String result=NetWorkUtils.postByHttpURL(IContants.REQUEST_WEATHER_URL, null);
+		Map<String,String> params=Collections.emptyMap();
+		params.put("cityname", EncodeUtils.urlEncoder(cityNo, "UTF-8"));
+		params.put("key", REQUEST_WEATHER_KEY);
+		String result=NetWorkUtils.postByHttpURL(REQUEST_WEATHER_URL, params);
 		if(StringUtils.isEmpty(result)) {
 			listener.OnError();
 		} else {
--- WeatherApp/src/com/ds/weatherapp/utils/IContants.java
+++ WeatherApp/src/com/ds/weatherapp/utils/IContants.java
@@ -1,5 +1,6 @@
 package com.ds.weatherapp.utils;
 
 public interface IContants {
-	public static final String REQUEST_WEATHER_URL="";
+	public static final String REQUEST_WEATHER_URL="http://op.juhe.cn/onebox/weather/query";
+	public static final String REQUEST_WEATHER_KEY="ef28310b96edaa78b7a2bcee9503b25f";
 }
